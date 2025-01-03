88f2561b430abca09e67dc8a6e5ed92e8fd88a65
Vidhi-Porwal <vidhi.porwal@gmail.com>
2025-01-03 19:30:33 +0530

    updated inputs

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-
 from .models import User, UserPlant, PlantInfo
 import google.generativeai as genai
 import os
