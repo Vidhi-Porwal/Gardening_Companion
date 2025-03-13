5855e54bde55e1e0c04b2b55c75c0fa77539530e
gurpreetkaur2309 <gurpreetkhanuja2309@gmail.com>
2025-03-13 13:56:01 +0530

    deafult garden will be created only if no garden exist

--- app/dashboard.py
+++ app/dashboard.py
@@ -64,7 +64,6 @@ def dashboard():
             garden_obj_id=ObjectId(default_garden_id)
         user_plants = list(db.garden_plant.find({"user_id": current_user.id, "garden_id": garden_obj_id}))
         user_plants_data = list(db.garden_plant.find({"user_id": current_user.id,"garden_id": garden_obj_id}, {"plant_id": 1, "_id": 0, "age_id": 1, "quantity": 1,}))
-        print(user_plants)
         plant_ids = [entry["plant_id"] for entry in user_plants_data]
         
 #        Fetch full plant details from plants collection
@@ -120,7 +119,7 @@ def dashboard():
                     plant_copy["age"] = age_info["age"]
                     plant_copy["age_id"] = age_info["age_id"]  # Fetch age
                     final_plants.append(plant_copy)
-        print(final_plants)
+        
 
 
         # Handle POST Requests
@@ -502,15 +501,13 @@ def dashboard():
 @dashboard_bp.route("/add_garden", methods=["POST"])
 def add_garden():
     garden_name = request.form.get("garden_name")
-    user_id = current_user.id  # Replace with your user session ID retrieval
-
-    # Fetch the MongoDB connection from the current app context
     db = current_app.config['DB_CONNECTION']
-
+    user_id = current_user.id
+    print(user_id)  # Replace with your user session ID retrieval
+    
     if garden_name and user_id:
         
         result=db.garden.insert_one({
-
             "gardenName": garden_name,
             "user_id": ObjectId(user_id),
             "created_at": datetime.now()
@@ -520,7 +517,6 @@ def add_garden():
         return redirect(url_for("dashboard.dashboard",garden_id=new_garden_id))
     return redirect(url_for("dashboard.dashboard"))
 
-
         
        
 
@@ -528,7 +524,7 @@ def add_garden():
 def ensure_default_garden(user_id):
     db = current_app.config['DB_CONNECTION']
     
-    default_garden = db.garden.find_one({"user_id": ObjectId(user_id), "gardenName": "My Garden"})
+    default_garden = db.garden.find_one({"user_id": ObjectId(user_id)})
     print(default_garden)
     if not default_garden:
         default_garden_id = db.garden.insert_one({
