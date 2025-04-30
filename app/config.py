7d1f3c980b73091a3a8919a8e194792ae20f97f8
t <ajeymalviya143@gmail.com>
2025-04-30 13:58:31 +0530

    added new file config which safes the client id and secrete on it and also google discovery url

---
+++ app/config.py
@@ -0,0 +1,3 @@
+GOOGLE_CLIENT_ID = '745426787344-1o3tnk39j9s84vtif2gbn8v277e0ks6r.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = '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'
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
