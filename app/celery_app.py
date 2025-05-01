929d53ab858f906a3bef3938558a8ca2d2f2c5ba
t <ajeymalviya143@gmail.com>
2025-05-01 15:26:00 +0530

    added new  functionality of autodiscover tasks

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,28 +1,9 @@
-# app/celery_app.py
-
 from celery import Celery
-from .config import Config
+from app.config import Config
 
 celery = Celery(__name__)
 celery.conf.broker_url = Config.CELERY_BROKER_URL
 celery.conf.result_backend = Config.CELERY_RESULT_BACKEND
 
-
-# def make_celery(app):
-
-#     celery = Celery(
-#         app.import_name,
-#         backend=app.config['CELERY_RESULT_BACKEND'],
-#         broker=app.config['CELERY_BROKER_URL']
-#     )
-#     celery.conf.update(app.config)
-#     celery.conf.beat_schedule = {}
-#     TaskBase = celery.Task
-
-#     class ContextTask(TaskBase):
-#         def __call__(self, *args, **kwargs):
-#             with app.app_context():
-#                 return TaskBase.__call__(self, *args, **kwargs)
-
-#     celery.Task = ContextTask
-#     return celery
+# 👇 This line ensures Celery knows your tasks
+celery.autodiscover_tasks(['app'])
