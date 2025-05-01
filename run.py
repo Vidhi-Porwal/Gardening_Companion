5e3cc48ebf5af7a62d57454f365a04e997eecd87
t <ajeymalviya143@gmail.com>
2025-05-01 15:14:48 +0530

    rearrange create app flask function in run file

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 from app import create_app
-
-
-from flask import Flask, request
-from flask_mail import Mail, Message
-from app.config import Config
-from app.tasks import send_async_email
 app = create_app()
-
 @app.after_request
 def add_header(response):
     # Disable caching of protected pages
@@ -15,24 +8,6 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-# print(app.url_map)
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-mail = Mail(app)
-
-@app.route('/send-email', methods=['POST'])
-def send_email():
-    data = request.get_json()
-    subject = data.get('subject')
-    recipients = data.get('recipients')
-    body = data.get('body')
-
-    send_async_email.delay(subject, recipients, body)
-
-    return {'message': 'Email is being sent!'}, 202
 
 
 if __name__ == '__main__':
